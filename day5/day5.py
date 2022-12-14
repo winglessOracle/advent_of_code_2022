b0723b6cfaade2d149840b182bba949e5983d46e
Carlo Wesseling <cwesseli@f0r6s11.codam.nl>
2022-12-14 14:25:14 +0100

    day7

--- day5/day5.py
+++ day5/day5.py
@@ -1,14 +1,24 @@
-import numpy as np
+import re
 
-#seperate and transpose first part
-x = 0
 stacks = []
-with open ('input5.txt', 'r') as file:
-	lines = file.read().split('\n')
-while x < 9:
-	stacks.insert(0, lines[x])
-	x += 1
-stacks_t = np.array(stacks)
+instructions = []
 
-print (stacks_t)
+file = open('input5.txt', 'r')
+for line in file:
+	if line == '\n':
+		break
+	stacks.append([line[k * 4 + 1] for k in range(len(line) // 4)])
+stacks.pop()
+stacks = zip(*stacks)
+stacks = ([list("".join(c).strip()[::-1]) for c in stacks])
 
+# for row in stacks:
+# 	print (row)
+
+for line in file:
+	move, source, dest = map(int, re.findall("\d+", line))
+	stacks[dest - 1].extend(stacks[source - 1][-move:][::-1])
+	stacks[source - 1] = stacks[source -1 ][:-move]
+
+output = ("".join([a[-1] for a in stacks]))
+print (output)
--- day7/day7.py
+++ day7/day7.py
@@ -1,4 +1,5 @@
 cur_dir = total = step = count = 0
+
 stackedpath = [0]
 with open ('input.txt', 'r') as file:
 	lines = file.read().strip().split()
@@ -7,18 +8,20 @@ for element in reversed(lines):
 		cur_dir += int(element)
 		if count != 0:
 			stackedpath[count] += int(cur_dir)
+	
 	if element == "ls":
 		print (cur_dir)
 		if count != 0:
 			cur_dir += int(stackedpath[count])
+			stackedpath.pop()
+			count -= 1
 		if cur_dir <= 100000:
 			total += cur_dir
 		cur_dir = 0
-	if element == "cd":
-		if count != 0:
-			stackedpath.pop()
-			count -= 1
+
 	if element == "..":
-		stackedpath.append(cur_dir)
+		stackedpath.append("..")
 		count += 1
+		print(stackedpath, count)
+
 print ("total:", total)
